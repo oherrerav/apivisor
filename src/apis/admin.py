3535dc9e7c6fc500026508ef0b80906946ec8547
Omar Herrera <oherrerav@gmail.com>
2016-02-12 00:10:49 -0500

    Request user filter in a modelAdmin

--- src/apis/admin.py
+++ src/apis/admin.py
@@ -4,31 +4,66 @@ from django.contrib import admin
 from .models import Api
 from .models import ChartVisualization
 from .models import ChartType
-from .models import Chart
 from .models import DashBoard
+from .models import Chart
 # from .models import SignUp
 
 # from .forms import SignUpForm
 
-class ApiModelAdmin(admin.ModelAdmin):
+class DashBoardAdmin(admin.ModelAdmin):
+	 def get_queryset(self, request):
+		 qs = super(DashBoardAdmin, self).get_queryset(request)
+		 if request.user.is_superuser:
+		     return qs
+		 return qs.filter(user=request.user)
+
+class ChartAdmin(admin.ModelAdmin):
+	 def get_queryset(self, request):
+		 qs = super(ChartAdmin, self).get_queryset(request)
+		 if request.user.is_superuser:
+		     return qs
+		 return qs.filter(user=request.user)
+
+class ApiAdmin(admin.ModelAdmin):
 	list_display = ["name","uri"]
 	# list_display_links =["uri"]
 	# list_filter =["name"]
 	list_editable = ["uri"]
-	search_fields = ["name","uri"]
-	class Meta:
-		model = Api
+	# search_fields = ["name","uri"]
+	# class Meta:
+	# 	model = Api
+	def get_queryset(self, request):
+		 qs = super(ApiAdmin, self).get_queryset(request)
+		 if request.user.is_superuser:
+		     return qs
+		 return qs.filter(user=request.user)
 
 # class SignUpAdmin(admin.ModelAdmin):
 # 	list_display = ["email","full_name","updated"]
 # 	form = SignUpForm
 # 	class Meta:
 # 		model = SignUp
+class ChartInline(admin.TabularInline):
+	model = DashBoard.charts.through
+
+	 #   qs = super(DashBoard, self).get_queryset(request)
+		# return qs.filter(user=request.user)
+
+	# class Meta:
+	# 	model = DashBoard
+		# .objects.filter(tag__name="DataWipTable")
 
 
-admin.site.register(Api,ApiModelAdmin)
+# class MyModelAdmin(admin.ModelAdmin):
+# 	def get_queryset(self, request):
+# 		qs = super(MyModelAdmin, self).get_queryset(request)
+# 		if request.user.is_superuser:
+# 			return qs
+# 		return qs.filter(author=request.user)
+
+admin.site.register(Api,ApiAdmin)
 # admin.site.register(SignUp,SignUpAdmin)
 admin.site.register(ChartVisualization)
 admin.site.register(ChartType)
-admin.site.register(Chart)
-admin.site.register(DashBoard)
+admin.site.register(DashBoard,DashBoardAdmin)
+admin.site.register(Chart,ChartAdmin)
--- src/apis/models.py
+++ src/apis/models.py
@@ -27,7 +27,7 @@ class ChartType (models.Model):
 	"""this model store the information for each ChartType"""
 	name = models.CharField(max_length=10)
 	status = models.BooleanField(default=1)
-	chartVisualization = models.ForeignKey(ChartVisualization)
+	chartVisualization = models.ForeignKey(ChartVisualization, on_delete=models.CASCADE)
 	status = models.BooleanField(default=1)
 	timestamp = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
@@ -36,21 +36,33 @@ class ChartType (models.Model):
 		return self.name
 
 class Chart(models.Model):
-	"""this model store the information for each Chart that you define"""
+	"""this is abstract model for Chart"""
+	CHART_SIZES = (
+        ('1', '1x'),
+        ('2', '2x'),
+        ('3', '3x'),
+    )
 	name = models.CharField(max_length=50)
-	chartType = models.ForeignKey(ChartType)
-	api = models.ForeignKey(Api)
+	chartType = models.ForeignKey(ChartType, on_delete=models.CASCADE)
+	api = models.ForeignKey(Api, on_delete=models.CASCADE)
+	size = models.CharField(help_text="size of your chart", max_length=2,choices=CHART_SIZES)
+	dataSet = models.CharField(max_length=20)
+	attribute = models.CharField(max_length=20)
+	dimensions = models.CharField(max_length=100)
+	options =  models.CharField(max_length=100)
 	status = models.BooleanField(default=1)
 	user = models.CharField(max_length=50)
 	timestamp = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
+
 	def __str__(self):
 		return self.name
 
+		
 class DashBoard(models.Model):
 	"""this model store the information for each DashBoard that you define"""
 	name = models.CharField(max_length=50)
-	chart = models.ForeignKey(Chart)
+	charts = models.ManyToManyField(Chart, through='chart')
 	default =  models.BooleanField()
 	status = models.BooleanField(default=1)
 	user = models.CharField(max_length=50)
